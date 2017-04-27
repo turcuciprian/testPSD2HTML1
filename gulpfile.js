dbf330bb1d4143c9e44c3f20df66d8a1fb5fccf8
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-27 13:15:44 +0300

    Removed css directory and it's contents, added the .gitignore condition for it since it is supposed to be there only on load time

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,5 @@ gulp.task('sass', function() {
 gulp.task('sass:watch', function() {
     gulp.watch('./src/scss/custom/*.scss', ['sass']);
     gulp.watch('./src/scss/*/*.scss', ['sass']);
-
 });
 gulp.task('default', ['sass', 'sass:watch']);
