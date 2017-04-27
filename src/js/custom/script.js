423066a60612081c385f25bd9a4c3720056cfca6
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-27 23:56:27 +0300

    completed loading template part with jquery implementation

--- src/js/custom/script.js
+++ src/js/custom/script.js
@@ -1,4 +1,15 @@
-jQuery(document).ready(function($){
-  var loginBtn = $('#loginBtn');
+jQuery(document).ready(function($) {
+    // main vars
+    var templates = {
+        'about': 'templates/about.html',
+        'settings': 'templates/settings.html',
+        'login': 'templates/login.html'
+    }
+    // obj vars
+    var mainContainer = $('.container');
+    var loginBtn = $('#loginBtn');
+    if (mainContainer[0]) {
+        mainContainer.load(templates['login']);
+    }
 
 });
