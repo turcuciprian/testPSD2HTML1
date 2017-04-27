1daecc36b3e788f26f19812bf3f31ce2990cbb58
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-27 23:23:00 +0300

    modified gulp config for js custom scripts and libs management

--- gulpfile.js
+++ gulpfile.js
@@ -18,11 +18,35 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('./css'));
 });
 //
+// Javascript
+//
+
+//custom
+gulp.task('compressjSCustom', function() {
+    return gulp.src('./src/javascript/custom/*.js')
+        .pipe(uglify('custom.min.js', {
+            outSourceMap: true,
+            mangle: false
+        }))
+        .pipe(gulp.dest('./js'));
+});
+//custom
+gulp.task('compressjSLib', function() {
+    return gulp.src('./src/javascript/lib/*.js')
+        .pipe(uglify('lib.min.js', {
+            outSourceMap: true,
+            mangle: false
+        }))
+        .pipe(gulp.dest('./js'));
+});
+//
 // First run files
 //
 
 gulp.task('sass:watch', function() {
     gulp.watch('./src/scss/custom/*.scss', ['sass']);
     gulp.watch('./src/scss/*/*.scss', ['sass']);
+    gulp.watch('./src/javascript/lib/*.js', ['compressjSLib']);
+    gulp.watch('./src/javascript/custom/*.js', ['compressjSCustom']);
 });
-gulp.task('default', ['sass', 'sass:watch']);
+gulp.task('default', ['sass', 'compressjSLib', 'compressjSCustom', 'sass:watch']);
