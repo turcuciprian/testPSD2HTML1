6dc0e569651881ebb68619fdc3acd4619dc71e76
Ciprian Turcu <turcuciprian1@gmail.com>
2017-04-28 10:30:58 +0300

    added custom made routing based on hashes in jquery

--- src/js/custom/script.js
+++ src/js/custom/script.js
@@ -1,30 +1,50 @@
 jQuery(document).ready(function($) {
-  $.ajaxSetup ({
-    // Disable caching of AJAX responses
-    cache: false
-});
+    $.ajaxSetup({
+        // Disable caching of AJAX responses
+        cache: false
+    });
+    var hash = $(location).attr('hash');
+    var path;
+
     // main vars
     var templates = {
         'about': 'templates/about.html',
         'settings': 'templates/settings.html',
         'login': 'templates/login.html'
     }
+
+
+    console.log(hash);
+
     // obj vars
     var mainContainer = $('.container');
     var mainBody = $('body');
 
     if (mainContainer[0]) {
-        mainContainer.load(templates['login'],function(){
-          mainBody.addClass('login');
-          loginBtn = $('#loginBtn');
-          if(loginBtn[0]){
-            loginBtn.click(function(){
-              mainContainer.load(templates['about'],function(){
-                mainBody.removeClass('login');
-              });
-            });
-          }
+      // custom made basic routing
+      switch (hash) {
+          case '#about':
+              path = templates['about'];
+              break;
+          case '#settings':
+              path = templates['settings'];
+              break;
+          default:
+              path = templates['login'];
+              mainBody.addClass('login');
+              break;
+      }
+
+        mainContainer.load(path, function() {
+            loginBtn = $('#loginBtn');
+            if (loginBtn[0]) {
+                loginBtn.click(function() {
+                    mainContainer.load(templates['about'], function() {
+                        mainBody.removeClass('login');
+                    });
+                });
+            }
         });
         //add the login class to the body to style the login page
-        }
+    }
 });
